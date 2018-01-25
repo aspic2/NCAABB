69b68c5df9ab25102d7c2870dbb197bce19e76b4
Thomp <thomp@protonmail.com>
2018-01-25 12:40:03 -0600

    turning off debug mode

--- app.py
+++ app.py
@@ -2,15 +2,12 @@
 # -*- coding: UTF-8 -*-
 
 """
-Defines the recommend view
+Run Flask App from here
 """
 
 from NCAABB import app
 import os
 
 if __name__ == '__main__':
-    #app.secret_key = 'whiskeystuff'
-    app.debug = True
-    #app.run(host='localhost', port=5000, debug=True)
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
